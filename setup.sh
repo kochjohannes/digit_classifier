e54ce02c68bdf5296ebe8269b62325d90c5f5f7d
kochjohannes <johannes.koch@live.se>
2018-01-21 21:30:39 +0100

    Change python to python3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting the setup, this will take approx. 10 min"
-python download_and_train.py;
+python3 download_and_train.py;
 echo "Removing downloaded files."
 rm mnist_train.csv; rm mnist_test.csv;
 echo "Setup done.";
